9f06b2c26682c85bacedfb2e94f230c6073beefc
Bentagai <bentagai@protonmail.com>
2023-09-21 13:42:20 +0100

    crea registro de usuario

--- app/page.js
+++ app/page.js
@@ -1,23 +1,14 @@
-import Image from 'next/image'
-import TailfyBackground from '../public/background.avif'
-import RegisterForm from '@/components/RegisterForm'
+import SignUp from '@/components/SignUp'
 
 export default async function Home() {
   const data = await fetch('http://localhost:3000/api/prices')
   const plans = await data.json()
 
   return (
-    <main className='h-screen relative'>
-      <Image
-        className='z-0 object-center object-cover'
-        src={TailfyBackground}
-        alt="Tailfy Brackground"
-        fill
-      />
-      <section className='h-full flex items-center justify-center'>
-        <div className='text-5xl text-red-500'>HOLA</div>
-        <RegisterForm {...plans} />
-      </section>
+    <main
+      className='bg-[url(../public/background.avif)] bg-no-repeat bg-cover bg-center w-full h-screen flex justify-center items-center'
+    >
+      <SignUp {...plans} />
     </main>
   )
 }
---
+++ components/SignUp.js
@@ -0,0 +1,111 @@
+'use client'
+
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import * as Yup from 'yup'
+
+export default function SignUp () {
+  // const [checked, setChecked] = useState(false)
+  // const [step, setStep] = useState(0)
+  // const [loading, setLoading] = useState(false)
+  // const price = props[0].unit_amount / 100
+
+  /* async function handleSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const formValues = Object.fromEntries(formData.entries())
+
+    try {
+      setLoading(true)
+      if (step === 1) {
+        const response = await fetch('/api/create-order', {
+          method: 'POST',
+          body: JSON.stringify({ ...formValues, priceId: props[0].id })
+        })
+
+        const { url } = await response.json()
+        window.location.href = url
+      } else {
+        const response = await fetch('http://localhost:1337/api/users', {
+          method: 'POST',
+          body: JSON.stringify({ ...formValues, role: 1 }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        setStep(1)
+
+      }
+    } catch (error) {
+      console.error('Error en crear el usuario:', error)
+    } finally {
+      setLoading(false)
+    }
+  } */
+
+  return (
+    <Formik
+      initialValues={
+        { username: '', email: '', password: '' }
+      }
+
+      validationSchema={
+        Yup.object({
+          username: Yup
+            .string()
+            .required('Your name is required'),
+          email: Yup
+            .string()
+            .email('Invalid email address')
+            .required('Email required'),
+          password: Yup // Mejorar esta validación, debería de tener números y letras?
+            .string()
+            .required('Campo obligatorio').min(6, 'Tu contraseña debe de tener como mínimo 6 carácteres.').matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+        })
+      }
+
+      onSubmit={
+        async (values, { resetForm }) => {
+          await fetch('http://localhost:1337/api/auth/local/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: values.username,
+              email: values.email,
+              password: values.password
+            })
+          })
+            .then(res => res.json())
+            .catch(error => console.error('Error:', error))
+          
+          resetForm()
+        }
+      }
+    >
+      <section
+       className="bg-white p-8 grid gap-4"
+      >
+        <h1 className="text-3xl text-center">Tailfy</h1>
+        <Form
+          className="flex flex-col gap-4"
+        >
+          <Field name="username" type="text" placeholder="Usuario" className="p-4 rounded-full drop-shadow-lg text-center" />{/* Name Field */}
+          <ErrorMessage name="username" component="span" className="pl-4 pt-2 text-sm" />
+
+          <Field name="email" type="email" placeholder="Email" className="p-4 rounded-full drop-shadow-lg text-center" />{/* Email Field */}
+          <ErrorMessage name="email" component="span" className="pl-4 pt-2 text-sm" />
+
+          <Field name="password" type="password" placeholder="Contraseña" className="p-4 rounded-full drop-shadow-lg text-center" />{/* Password Field */}
+          <ErrorMessage name="password" component="span" className="pl-4 pt-2 text-sm" />
+
+          <button
+            type="submit"
+            className="text-base leading-none bg-blue-500 rounded-full px-6 py-4 w-fit mx-auto"
+          > Crear Cuenta
+          </button>
+        </Form>
+      </section>
+    </Formik >
+  )
+}
